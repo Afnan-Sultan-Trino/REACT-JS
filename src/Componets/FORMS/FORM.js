1d05c7736309e28a72d29e882d111cdd1edc33ee
Afnan-Sultan-Trino <asultantrino@gmail.com>
2026-06-13 20:48:49 +0600

    using object and 1 event handler

--- src/Componets/FORMS/FORM.js
+++ src/Componets/FORMS/FORM.js
@@ -3,35 +3,65 @@ import style from './form.module.css'
 
 export default function FORM() {
 
-    const[name,setName]=useState('')
-    const[email,setEmail]=useState('')
-    const[password,setPassword]=useState('')
+    /*
+    const[name,setName]=useState('')     //defining state
+    const[email,setEmail]=useState('')     //defining state
+    const[password,setPassword]=useState('')   //defining state
+
+    */
+
+    //using useState object
+
+    const[user,setUser]=useState({
+        name:'',
+        email:'',
+        password:''
+    })
+
+    //destructor calling , otherwise we have to call it as user.name, user.email. But now, only name,email
+
+    const{name,email,password}= user; 
 
 
 
+   /* creating 3 individual event handler
 
-    const handleChange=(e)=>{
-        setName(e.target.value)
+    const handlNameChange=(e)=>{
+        setUser({name:e.target.value,email,password})
     }
 
     const handleEmailChange=(e)=>{
-        setEmail(e.target.value)
+        setUser({name,email:e.target.value,password})
     }
 
     const handlePasswordChange=(e)=>{
-        setPassword(e.target.value)
-    }
+        setUser({name,email,password:e.target.value})
+    } 
+        */
+
+    
+    //creating only 1 event handler
+    const handleChange=(e)=>{
+        
+            setUser({...user,[e.target.name]:e.target.value})
+        }
+
+
+
 
     const handleSubmit=(e)=>{
         console.log("FORM is submitted")
         
+        /*
         let userinfo={  //creating object
             name,       // if the state's varibale name and object's attribute name is same, we can use only state's variable name
             email,
             password
         }
+            */
 
-        console.log(userinfo)
+
+        console.log(user)
         e.preventDefault()
     }
 
@@ -47,12 +77,12 @@ export default function FORM() {
         <br></br>
 
         <label>Email : </label>
-        <input type='email' name='email'id='email'value={email} onChange={handleEmailChange}  required></input>
+        <input type='email' name='email'id='email'value={email} onChange={handleChange}  required></input>
         <br></br>
         <br></br>
 
         <label>Password : </label>
-        <input type='password' name='password'id='password'value={password} onChange={handlePasswordChange}  required></input>
+        <input type='password' name='password'id='password'value={password} onChange={handleChange}  required></input>
         <br></br>
         <br></br>
 
